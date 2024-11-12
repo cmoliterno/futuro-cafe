0e5f1224aa61ddd8d82f42a1ac3cc513e10e5ac0
cmoliterno <carolinemoliterno@gmail.com>
2024-11-12 16:30:36 -0300

    Fix deploy ambiente

--- fc-web-fe/src/services/api.ts
+++ fc-web-fe/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.BASE_URL || 'http://localhost:3000/api';
+const API_URL = process.env.BASE_URL || 'https://api-futurocafe.qwize.io/api/';
 
 const api = axios.create({
     baseURL: API_URL,
