2d4bea4b8a9b521fb7e2f67c49dc66a740e86bfb
cmoliterno <carolinemoliterno@gmail.com>
2024-11-12 16:42:44 -0300

    Fix deploy ambiente

--- fc-web-be/src/config/config.ts
+++ fc-web-be/src/config/config.ts
@@ -1,13 +1,28 @@
-require('dotenv').config(); // Carrega as variáveis de ambiente do .env
+import { Dialect } from 'sequelize';
 
-module.exports = {
+interface DBConfig {
+    username: string | undefined;
+    password: string | undefined;
+    database: string | undefined;
+    host: string | undefined;
+    dialect: Dialect;
+    port: number;
+    dialectOptions: {
+        options: {
+            encrypt: boolean;
+            enableArithAbort: boolean;
+        };
+    };
+}
+
+const config: { development: DBConfig; production: DBConfig } = {
     development: {
         username: process.env.DB_USER,
         password: process.env.DB_PASS,
         database: process.env.DB_NAME,
         host: process.env.DB_HOST,
         dialect: 'mssql',
-        port: parseInt(process.env.DB_PORT, 10),
+        port: parseInt(process.env.DB_PORT ?? '3000', 10), // fallback para a porta padrão 1433
         dialectOptions: {
             options: {
                 encrypt: true,
@@ -21,7 +36,7 @@ module.exports = {
         database: process.env.DB_NAME,
         host: process.env.DB_HOST,
         dialect: 'mssql',
-        port: parseInt(process.env.DB_PORT, 10),
+        port: parseInt(process.env.DB_PORT ?? '3000', 10),
         dialectOptions: {
             options: {
                 encrypt: true,
@@ -30,3 +45,5 @@ module.exports = {
         }
     }
 };
+
+export default config;
