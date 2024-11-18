608e387d3604f38a1abc61047f535fa05f244d1e
cmoliterno <carolinemoliterno@gmail.com>
2024-11-18 16:36:08 -0300

    Fix URL BE

--- fc-web-fe/src/services/api.ts
+++ fc-web-fe/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.BASE_URL || 'http://52.91.164.75:3000/api/'; //'https://api-futurocafe.qwize.io/api/';
+const API_URL = process.env.BASE_URL || 'https://futurocafe.qwize.io/api/';
 
 const api = axios.create({
     baseURL: API_URL,
